7c966ec70362b2a6a7cdad0b8ba61f49ab078b70
rdonii <r.donii@mail.ru>
2023-06-12 06:00:35 -0700

    Category Income Outcome models added

--- casher/models.py
+++ casher/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    class TypeChoice(models.TextChoices):
+        OUT = "OUT"
+        IN = "IN"
+
+    name = models.CharField(max_length=25, db_index=True, unique=True)
+    type = models.CharField(max_length=3, choices=TypeChoice.choices)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Income(models.Model):
+    class CurrencyChoice(models.TextChoices):
+        USD = "USD"
+        UZS = "UZS"
+
+    person = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    currency = models.CharField(max_length=3, choices=CurrencyChoice.choices)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Outcome(models.Model):
+    class CurrencyChoice(models.TextChoices):
+        USD = "USD"
+        UZS = "UZS"
+
+    person = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    currency = models.CharField(max_length=3, choices=CurrencyChoice.choices)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
