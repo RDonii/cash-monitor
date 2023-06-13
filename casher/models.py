655d67e9e31350fed63768c0e85698f68229605c
rdonii <r.donii@mail.ru>
2023-06-13 00:25:32 -0700

    models updated

--- casher/models.py
+++ casher/models.py
@@ -13,12 +13,15 @@ class Category(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Action(models.Model):
 
     issuer = models.CharField(max_length=60)
-    amount_dollar = models.DecimalField(max_digits=11, decimal_places=2)
-    amount_sum = models.DecimalField(max_digits=11, decimal_places=2)
+    amount_dollar = models.DecimalField(max_digits=11, decimal_places=2, default=0)
+    amount_sum = models.DecimalField(max_digits=11, decimal_places=2, default=0)
     description = models.CharField(max_length=255, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.RESTRICT, db_index=True)
     dolar_price = models.DecimalField(max_digits=11, decimal_places=2)
