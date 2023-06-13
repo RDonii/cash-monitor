9065360a19f3f820edccb701784dfa6e0ac46b38
rdonii <r.donii@mail.ru>
2023-06-13 04:30:45 -0700

    filters and orders added

--- casher/views.py
+++ casher/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from casher.serializers import CategorySerializer, ActionSerilizer
 from casher.models import Category, Action
@@ -7,6 +9,9 @@ from casher.models import Category, Action
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['type']
+    search_fields = ['name']
     
     def get_queryset(self):
         self.queryset = self.queryset.filter(user=self.request.user)
@@ -19,6 +24,9 @@ class CategoryViewSet(ModelViewSet):
 class ActionViewSet(ModelViewSet):
     queryset = Action.objects.select_related('category').all()
     serializer_class = ActionSerilizer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['category', 'category__type']
+    search_fields = ['category__name', 'issuer']
 
     def get_queryset(self):
         self.queryset = self.queryset.filter(category__user=self.request.user)
