95b5bfbb9f4677fbcbb29c277af700a5fc13ec74
rdonii <r.donii@mail.ru>
2023-06-13 00:25:23 -0700

    serializers updated

--- casher/serializers.py
+++ casher/serializers.py
@@ -8,12 +8,22 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'type', 'user', 'created']
+        read_only_fields = ['id', 'user']
 
 
 class ActionSerilizer(serializers.ModelSerializer):
-    type = serializers.CharField(source='category__type')
-    name = serializers.CharField(source='category__name')
+    type = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
 
     class Meta:
         model = Action
-        fields = ['id', 'issuer', "amount_dollar", "amount_sum", "description", "dolar_price", "issued", "name", "type", "created", "updated"]
+        fields = ['id', 'category', "name", "type", 'issuer', "amount_dollar", "amount_sum", "description", "dolar_price", "issued", "created", "updated"]
+        extra_kwargs = {
+            'category': {"write_only": True}
+        }
+
+    def get_type(self, action: Action):
+        return action.category.type
+
+    def get_name(self, action: Action):
+        return action.category.name
