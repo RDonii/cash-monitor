9527d31eb2a389688d01bb9dd53934e944ab7246
rdonii <r.donii@mail.ru>
2023-06-19 17:56:09 +0500

    host conf added

--- cash_monitor/settings.py
+++ cash_monitor/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] + env.str('HOST', '')
 
 if DEBUG:
     import socket  # only if you haven't already imported this
