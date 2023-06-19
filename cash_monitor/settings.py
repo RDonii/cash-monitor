4b50bc274c6b46604d25f93fb9d0e7381d9ef3b3
rdonii <r.donii@mail.ru>
2023-06-19 16:55:58 +0500

    media static urls updated

--- cash_monitor/settings.py
+++ cash_monitor/settings.py
@@ -142,7 +142,9 @@ STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_DIR = 'media/'
+
+MEDIA_ROOT = BASE_DIR / MEDIA_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
